072d19e1be777920cf98d491b42b38a2521cfba3
aghadaniyal <aghadaniyal.tariq@gmail.com>
2019-02-26 16:56:44 +0500

    edit, update,table added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,17 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :destroy,:show]
+
+
 	def index
 		@articles = Article.all
 	end
-	def new
 
+	def new
 		@article = Article.new
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+	
 	end
 
 	def create
@@ -17,7 +20,7 @@ class ArticlesController < ApplicationController
 		
 		if @article.save
 		flash[:notice] = "Article was successfully created"
-		redirect_to article_path(@article)
+		redirect_to articles_path
 		
 		else
 		render 'new'
@@ -25,23 +28,34 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-
-		@article = Article.find(params[:id])
 		
 	end
 
 	def update
-		@article = Article.find(params[:id])
+		
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully edited"
-			redirect_to article_path(@article)
+			redirect_to articles_path
 		else
 			render 'edit'
 		end
 
 	end
 
-	private  def article_params
+	def destroy
+		
+		@article.destroy
+			flash[:notice] = "Article was successfully deleted"
+			redirect_to articles_path
+	end
+
+	private  
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+
+	def article_params
 
 		params.require(:article).permit(:title,:description)
 	
